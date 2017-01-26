0e88bb850f48f0cdf6a8198978a3ae6897b06e42
Adrian Gebhart <adrian.binich@live.de>
2017-01-26 14:49:42 +0100

    Updated the install script to download the correct vsix-file

--- .appveyor/install-vsix.ps1
+++ .appveyor/install-vsix.ps1
@@ -1,4 +1,4 @@
-$vsixPath = "$($env:USERPROFILE)\sqlite-wp81-winrt-3081002.vsix"
-(New-Object Net.WebClient).DownloadFile('https://visualstudiogallery.msdn.microsoft.com/5d97faf6-39e3-4048-a0bc-adde2af75d1b/file/132406/15/sqlite-wp81-winrt-3081002.vsix', $vsixPath)
-"`"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
+$vsixPath = "$($env:USERPROFILE)\sqlite-uwp-3150200.vsix"
+(New-Object Net.WebClient).DownloadFile('https://www.sqlite.org/2017/sqlite-uwp-3150200.vsix', $vsixPath)
+"`"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 & .\install-vsix.cmd
