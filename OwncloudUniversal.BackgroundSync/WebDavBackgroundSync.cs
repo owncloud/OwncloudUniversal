2d8b460b43d6cf6213a66c7bedd173834cba2279
Adrian Gebhart <adrian.binich@live.de>
2017-01-31 22:46:54 +0100

    Fixed BackgroundTaskCancellation (it worked fine but the certification kit did not like it)

--- OwncloudUniversal.BackgroundSync/WebDavBackgroundSync.cs
+++ OwncloudUniversal.BackgroundSync/WebDavBackgroundSync.cs
@@ -15,25 +15,28 @@ namespace OwncloudUniversal.BackgroundSync
     public sealed class WebDavBackgroundSync : IBackgroundTask
     {
         BackgroundTaskDeferral _deferral;
+        private BackgroundSyncProcess _worker;
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
+
+            taskInstance.Canceled += OnCanceled;
             _deferral = taskInstance.GetDeferral();
-            
-            var fileSystem = new FileSystemAdapter(true, null);
-            var webDav = new WebDavAdapter(true, Configuration.ServerUrl, Configuration.Credential, fileSystem);
-            fileSystem.LinkedAdapter = webDav;
-            var worker = new BackgroundSyncProcess(fileSystem, webDav, true);
+
             try
             {
-                await worker.Run();
+                var fileSystem = new FileSystemAdapter(true, null);
+                var webDav = new WebDavAdapter(true, Configuration.ServerUrl, Configuration.Credential, fileSystem);
+                fileSystem.LinkedAdapter = webDav;
+                _worker = new BackgroundSyncProcess(fileSystem, webDav, true);
+                await _worker.Run();
             }
             catch (Exception e)
             {
                 await LogHelper.Write($"BackgroundTask Exception: {e.Message}" + Environment.NewLine +
                                       $"{e.StackTrace}" + Environment.NewLine +
-                                      $"Status:{worker.ExecutionContext.Status.ToString()}" + Environment.NewLine +
-                                      $"File: {worker.ExecutionContext.CurrentFileNumber}" + Environment.NewLine +
-                                      $"of {worker.ExecutionContext.TotalFileCount}");
+                                      $"Status:{_worker.ExecutionContext.Status.ToString()}" + Environment.NewLine +
+                                      $"File: {_worker.ExecutionContext.CurrentFileNumber}" + Environment.NewLine +
+                                      $"of {_worker.ExecutionContext.TotalFileCount}");
             }
             finally
             {
@@ -41,13 +44,17 @@ namespace OwncloudUniversal.BackgroundSync
                 await LogHelper.Write("BackgroundTask finished");
                 _deferral.Complete();
             }
-
-            taskInstance.Canceled += (instance, reason) =>
-            {
-                Task.Run(() => LogHelper.Write($"BackgroundTask canceled. Reason: {reason}, Status:{worker.ExecutionContext.Status.ToString()} File: {worker.ExecutionContext.CurrentFileNumber} of {worker.ExecutionContext.TotalFileCount}"));
-            };
-
-
         }
+
+        private void OnCanceled(IBackgroundTaskInstance instance, BackgroundTaskCancellationReason reason)
+        {
+            if (_worker != null)
+            {
+                _worker.ExecutionContext.Status = ExecutionStatus.Stopped;
+                Task.Run(() =>LogHelper.Write($"BackgroundTask canceled. Reason: {reason}, Status:{_worker.ExecutionContext.Status.ToString()} File: {_worker.ExecutionContext.CurrentFileNumber} of {_worker.ExecutionContext.TotalFileCount}"));
+            }
+        }
+
+
     }
 }
