a5452e1e5b4bdf47d3bd156eca242b6c4d5046c3
Adrian Gebhart <adrian.binich@live.de>
2017-03-04 14:11:00 +0100

    Added basic status info based on a live database query

--- OwncloudUniversal/Converters/ItemToSyncStatusConverter.cs
+++ OwncloudUniversal/Converters/ItemToSyncStatusConverter.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
+using OwncloudUniversal.Model;
 using OwncloudUniversal.Shared.Model;
 using OwncloudUniversal.WebDav;
 using OwncloudUniversal.WebDav.Model;
@@ -18,13 +19,33 @@ namespace OwncloudUniversal.Converters
             var davItem = value as BaseItem;
             if (davItem != null)
             {
-                if (davItem.IsSynced)
-                    return davItem.IsCollection ? Symbol.SyncFolder : Symbol.Page2;
-                return davItem.IsCollection ? Symbol.Folder : Symbol.Page;
+                var dbItem = ItemTableModel.GetDefault().GetItem(davItem);
+                if (dbItem == null)
+                {
+                    foreach (var folderAssociation in FolderAssociationTableModel.GetDefault().GetAllItems())
+                    {
+                        if (davItem.EntityId.Contains(folderAssociation.RemoteFolderFolderPath))
+                            return "/Assets/SyncIcons/ic_synchronizing.png";//not in index yet
+                    }
+                    return null;
+                }
 
+                var link = LinkStatusTableModel.GetDefault().GetItem(dbItem);
+                if (link == null)
+                    return "/Assets/SyncIcons/ic_synchronizing.png";//indexed but not synced
+
+                var linkedItemId = dbItem.Id == link.SourceItemId ? link.TargetItemId : link.SourceItemId;
+                var linkedItem = ItemTableModel.GetDefault().GetItem(linkedItemId);
+                if(linkedItem.ChangeNumber != link.ChangeNumber)
+                    return "/Assets/SyncIcons/ic_synchronizing.png";
+
+
+                if (davItem.ChangeKey == dbItem.ChangeKey && link.ChangeNumber == dbItem.ChangeNumber)
+                    return "/Assets/SyncIcons/ic_synced.png";
+                
+                return "/Assets/SyncIcons/ic_synchronizing.png";
             }
-            
-            return Symbol.Help;
+            return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
