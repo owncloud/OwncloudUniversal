f9c6589094caabdf9076b6222791738b839272d3
Adrian Gebhart <adrian.binich@live.de>
2017-07-09 20:52:17 +0200

    Add UI to configure camera upload

---
+++ src/OwncloudUniversal/ViewModels/CameraUploadPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+using OwncloudUniversal.Synchronization.Configuration;
+using Template10.Mvvm;
+
+namespace OwncloudUniversal.ViewModels
+{
+    class CameraUploadPageViewModel : ViewModelBase
+    {
+        private readonly InstantUploadRegistration _registration;
+
+        public CameraUploadPageViewModel()
+        {
+            _registration = new InstantUploadRegistration();
+        }
+
+        public bool Enabled
+        {
+            get => _registration.Enabled;
+            set
+            {
+                _registration.Enabled = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
